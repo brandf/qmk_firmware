81eb43a3d7745b48b5fd24e0b04f054214c87776
brandf <brandf@fb.com>
2022-01-17 20:46:22 -0800

    adding keymap

--- keyboards/osprette/keymaps/brandf/keymap.c
+++ keyboards/osprette/keymaps/brandf/keymap.c
@@ -18,101 +18,51 @@
 #include "quantum.h"
 
 enum layers {
-    _MAIN = 0,
-    _SYM_LMOD,
-    _NUM_RMOD,
+    _COLEMAK = 0,
+    /*_QWERTY,
+    _GAME,*/
     _NAV,
-    _FN
+    _NUM,
+    _SYM,
+    _FUN
 };
 
-// layer controls
-#define LMOD    OSL(_SYM_LMOD)
-#define RMOD    OSL(_NUM_RMOD)
-#define FN      OSL(_FN)
-#define SPC_NAV LT(_NAV, KC_SPC)
+#define U_RDO C(KC_Y)
+#define U_PST C(KC_V)
+#define U_CPY C(KC_C)
+#define U_CUT C(KC_X)
+#define U_UND C(KC_Z)
 
-// one-shot mods
-#define LSFT_ OSM(MOD_LSFT)
-#define RSFT_ OSM(MOD_RSFT)
-#define LALT_ OSM(MOD_LALT)
-#define RALT_ OSM(MOD_RALT)
-#define LCTL_ OSM(MOD_LCTL)
-#define RCTL_ OSM(MOD_RCTL)
-#define LGUI_ OSM(MOD_LGUI)
-#define RGUI_ OSM(MOD_RGUI)
-#define RSG_  OSM(MOD_RSFT | MOD_RGUI)
-#define RAG_  OSM(MOD_RALT | MOD_RGUI)
-#define RCA_  OSM(MOD_RCTL | MOD_RALT)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_MAIN] = LAYOUT(
-                     KC_P,  KC_L,  KC_C, KC_F,         KC_SLSH,  KC_DOT, KC_QUOT, KC_SCLN,
-        KC_V, KC_R,  KC_N,  KC_T,  KC_S, KC_D,         KC_COMM,  KC_A,   KC_E,    KC_I,    KC_H, KC_Q,
-              KC_Z,  KC_B,  KC_M,  KC_G, KC_W,         KC_MINUS, KC_U,   KC_O,    KC_Y,    KC_K,
-                                   LMOD, SPC_NAV,      RSFT_,    RMOD
+    [_COLEMAK] = LAYOUT(
+                                 KC_W,          KC_F,          KC_P,              KC_B,                     KC_J,               KC_L,               KC_U,             KC_Y,
+        KC_Q,     LGUI_T(KC_A),  LALT_T(KC_R),  LCTL_T(KC_S),  LSFT_T(KC_T),      KC_G,                     KC_M,               RSFT_T(KC_N),       RCTL_T(KC_E),     RALT_T(KC_I),         RGUI_T(KC_O),    KC_SCLN,
+                  KC_Z,          KC_X,          KC_C,          KC_D,              KC_V,                     KC_K,               KC_H,               KC_COMM,          KC_DOT,               KC_SLSH,
+                                                               LT(_NUM, KC_TAB),  LT(_NAV, KC_SPC),         LT(_SYM, KC_BSPC),  LT(_FUN, KC_ENT)
     ),
-
-    [_SYM_LMOD] = LAYOUT(
-                        KC_NO,   C(S(KC_TAB)), C(KC_TAB),  KC_NO,            S(KC_LBRC), KC_DEL,   S(KC_RBRC), S(KC_GRV),
-        KC_CAPS, LSFT_, LALT_,   LCTL_,        LGUI_,      KC_PSCR,          S(KC_9),    KC_BSPC,  S(KC_0),    S(KC_COMM), S(KC_3), S(KC_2),
-                 KC_NO, C(KC_W), C(KC_Q),      A(KC_LEFT), A(KC_RIGHT),      KC_LBRC,    KC_ENTER, KC_RBRC,    S(KC_DOT),  KC_BSLS,
-                                               KC_NO,      KC_NO,            KC_TRNS,    FN
-    ),
-
-    [_NUM_RMOD] = LAYOUT(
-                             KC_7,  KC_8,  KC_9,   S(KC_8),        KC_NO, RSG_,    KC_NO,   KC_NO,
-        S(KC_BSLS), S(KC_7), KC_1,  KC_2,  KC_3,   KC_EQL,         KC_NO, RGUI_,   RCTL_,   RALT_,   RSFT_, KC_NO,
-                    S(KC_4), KC_4,  KC_5,  KC_6,   S(KC_EQL),      KC_NO, C(KC_C), C(KC_D), C(KC_V), KC_NO,
-                                           KC_DOT, KC_0,           KC_NO, KC_NO
-    ),
-
     [_NAV] = LAYOUT(
-                          KC_VOLD, KC_VOLU, KC_MNXT,  KC_MUTE,      RALT_,   G(KC_D),     G(S(KC_D)), G(KC_Q),
-        KC_MPRV, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_MPLY,      RAG_,    G(KC_LEFT),  G(KC_DOWN), G(KC_UP), G(KC_RIGHT), KC_NO,
-                 KC_HOME, KC_PGDN, KC_PGUP, KC_END,   KC_MSTP,      RCTL_,   G(KC_ENTER), G(KC_O),    G(KC_N),  KC_NO,
-                                            KC_NO,    KC_NO,        KC_TRNS, RCA_
+                                 KC_NO,         KC_NO,         KC_NO,             KC_NO,                    KC_INS,             KC_PGUP,            KC_UP,            KC_NO,
+        KC_NO,    KC_LGUI,       KC_LALT,       KC_LCTL,       KC_LSFT,           KC_NO,                    KC_HOME,            KC_LEFT,            KC_DOWN,          KC_RIGHT,             KC_END,          KC_NO,
+                  U_UND,         U_CUT,         U_CPY,         U_PST,             U_RDO,                    KC_NO,              KC_PGDN,            KC_NO,            KC_NO,                KC_NO,
+                                                               KC_NO,             KC_NO,                    KC_BSPC,            KC_ENT
     ),
-
-    [_FN] = LAYOUT(
-                      KC_F7, KC_F8, KC_F9, KC_F10,      KC_NO,   KC_BRID, KC_BRIU, KC_NO,
-        KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F11,      KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO,
-               KC_NO, KC_F4, KC_F5, KC_F6, KC_F12,      KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
-                                    KC_NO, KC_NO,       KC_TRNS, KC_NO
+    [_NUM] = LAYOUT(
+                                 KC_NO,         KC_NO,         KC_NO,             KC_NO,                    KC_PSLS,            KC_P7,              KC_P8,            KC_P9,
+        KC_NO,    KC_LGUI,       KC_LALT,       KC_LCTL,       KC_LSFT,           KC_NO,                    KC_COMM,            KC_P4,              KC_P5,            KC_P6,                KC_PPLS,        KC_PMNS,
+                  U_UND,         U_CUT,         U_CPY,         U_PST,             U_RDO,                    KC_PDOT,            KC_P1,              KC_P2,            KC_P3,                KC_PAST,
+                                                               KC_NO,             KC_NO,                    KC_0,               KC_PENT
     ),
-};
-
-enum combos {
-    PC_J,
-    BG_X,
-    BM_ESCAPE,
-    MG_TAB,
-    DQ_GRAVE,
-    QS_PERCENT,
-};
-
-const uint16_t PROGMEM j_combo[] = {KC_P, KC_C, COMBO_END};
-const uint16_t PROGMEM x_combo[] = {KC_B, KC_G, COMBO_END};
-const uint16_t PROGMEM escape_combo[] = {KC_B, KC_M, COMBO_END};
-const uint16_t PROGMEM tab_combo[] = {KC_M, KC_G, COMBO_END};
-const uint16_t PROGMEM grave_combo[] = {KC_DOT, KC_QUOT, COMBO_END};
-const uint16_t PROGMEM percent_combo[] = {KC_QUOT, KC_SCLN, COMBO_END};
-
-combo_t key_combos[COMBO_COUNT] = {
-    [PC_J]       = COMBO(j_combo, KC_J),
-    [BG_X]       = COMBO(x_combo, KC_X),
-    [BM_ESCAPE]  = COMBO(escape_combo, KC_ESC),
-    [MG_TAB]     = COMBO(tab_combo, S(KC_TAB)),
-    [DQ_GRAVE]   = COMBO(grave_combo, KC_GRAVE),
-    [QS_PERCENT] = COMBO(percent_combo, S(KC_5)),
-};
-
-const key_override_t question_mark_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, S(KC_SLSH));
-const key_override_t exclamation_mark_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMM, S(KC_1));
-const key_override_t forward_slash_override = ko_make_basic(MOD_MASK_SHIFT, KC_SLSH, S(KC_6));
-
-const key_override_t **key_overrides = (const key_override_t *[]){
-    &question_mark_override,
-    &exclamation_mark_override,
-    &forward_slash_override,
-    NULL
+    [_SYM] = LAYOUT(
+                                 S(KC_COMM),    S(KC_9),       S(KC_0),           S(KC_DOT),                S(KC_1),            S(KC_2),            S(KC_3),          S(KC_4),
+        KC_ESC,   KC_SLSH,       KC_QUOT,       S(KC_LBRC),    S(KC_RBRC),        S(KC_QUOT),               S(KC_BSLS),         KC_RSFT,            KC_RCTL,          KC_RALT,              KC_RGUI,        S(KC_5),
+                  KC_BSLS,       KC_GRV,        KC_LBRC,       KC_RBRC,           S(KC_GRV),                S(KC_7),            S(KC_MINS),         KC_NO,            KC_NO,                S(KC_6),
+                                                               KC_TAB,            KC_SPC,                   KC_NO,              KC_NO
+    ),
+    [_FUN] = LAYOUT(
+                                 KC_F7,         KC_F8,         KC_F9,             KC_PSCR,                  KC_MUTE,            KC_VOLD,            KC_VOLU,          KC_MPLY,
+        KC_F12,   KC_F11,        KC_F4,         KC_F5,         KC_F6,             KC_SCRL,                  KC_NO,              KC_RSFT,            KC_RCTL,          KC_RALT,              KC_RGUI,         KC_MEDIA_STOP,
+                  KC_F10,        KC_F1,         KC_F2,         KC_F3,             KC_PAUS,                  KC_NO,              KC_MRWD,            KC_MPRV,          KC_MNXT,              KC_MFFD,
+                                                               KC_TAB,            KC_SPC,                   KC_NO,              KC_NO
+    ),
 };
